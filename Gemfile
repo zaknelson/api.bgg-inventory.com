2094af311a5e0e33ed4af796c7925c78ace5ff76
Zak Nelson <zak.nelson@gmail.com>
2013-09-24 23:50:26 -0700

    Update basic api server.

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@ ruby '2.0.0'
 
 gem "json", "1.8.0"
 gem "nokogiri", "1.5.6"
+gem "puma", "2.4.0"
 gem "sinatra", "1.4.2"
--- server.rb
+++ server.rb
@@ -1,5 +1,15 @@
 require "sinatra"
 
 get "/" do
-  "Hello World!"
+	json_exists = File.exists?("public/api/v1/games.json")
+	json_modified_time = json_exists ? File.mtime("public/api/v1/games.json") : nil
+	scrape_started_long_ago = !File.exists?("scrape-started.txt") || (Time.now - File.mtime("scrape-started.txt") > 60 * 60)
+	scrape_finished_long_ago = !json_exists || (Time.now - json_modified_time > 60 * 60 * 8)
+	if scrape_started_long_ago && scrape_finished_long_ago
+		Thread.new do
+			FileUtils.touch("scrape-started.txt")
+			puts `ruby bgg-inventory-scraper.rb`
+		end
+	end
+	json_exists ? json_modified_time.to_s : ""
 end
