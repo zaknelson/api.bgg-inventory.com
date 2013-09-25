80232a7385b814dc7e2437d50d0c45995a56c1b5
Zak Nelson <zak.nelson@gmail.com>
2013-09-25 00:29:44 -0700

    Set header in after filter.

--- server.rb
+++ server.rb
@@ -1,7 +1,5 @@
 require "sinatra"
 
-games_json = File.read("public/api/v1/games.json")
-
 get "/" do
 	json_exists = File.exists?("public/api/v1/games.json")
 	json_modified_time = json_exists ? File.mtime("public/api/v1/games.json") : nil
@@ -11,13 +9,12 @@ get "/" do
 		Thread.new do
 			FileUtils.touch("scrape-started.txt")
 			puts `ruby bgg-inventory-scraper.rb`
-			games_json = File.read("public/api/v1/games.json")
 		end
 	end
 	json_exists ? json_modified_time.to_s : ""
 end
 
-get "/api/v1/games.json" do
+after do
+	puts "after"
 	response.headers['Access-Control-Allow-Origin'] = "http://bgg-inventory.com"
-	games_json
 end
