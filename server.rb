a4b70c6877899e93a4847eaa63d331760174cb7e
Zak Nelson <zak.nelson@gmail.com>
2013-09-25 09:28:20 -0700

    Try scraping again in 30 minutes if there was a failure.

--- server.rb
+++ server.rb
@@ -3,7 +3,7 @@ require "sinatra"
 get "/" do
 	json_exists = File.exists?("games.json")
 	json_modified_time = json_exists ? File.mtime("games.json") : nil
-	scrape_started_long_ago = !File.exists?("scrape-started.txt") || (Time.now - File.mtime("scrape-started.txt") > 60 * 60)
+	scrape_started_long_ago = !File.exists?("scrape-started.txt") || (Time.now - File.mtime("scrape-started.txt") > 60 * 30)
 	scrape_finished_long_ago = !json_exists || (Time.now - json_modified_time > 60 * 60 * 8)
 	if scrape_started_long_ago && scrape_finished_long_ago
 		Thread.new do
